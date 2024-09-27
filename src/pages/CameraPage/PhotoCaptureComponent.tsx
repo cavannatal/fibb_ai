626badb8a12bdae4a424652806982478e644bebf
ceaga90 <ceaga90@gmail.com>
2024-09-26 23:38:58 -0500

    Update PhotoCaptureComponent.tsx

--- src/pages/CameraPage/PhotoCaptureComponent.tsx
+++ src/pages/CameraPage/PhotoCaptureComponent.tsx
@@ -227,7 +227,7 @@ const PhotoCaptureComponent: React.FC = () => {
         const blob = await response.blob();
 
         // Requesting the presigned URL from the Lambda function
-        const presignedUrlResponse = await fetch('https://wpslia2qcj.execute-api.us-east-2.amazonaws.com/prod/photo-upload-url', {
+        const presignedUrlResponse = await fetch('https://rn3fz2qkeatimhczxdtivhxne40lnkhr.lambda-url.us-east-2.on.aws/', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
