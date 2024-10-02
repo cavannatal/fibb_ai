2129bc2a720b32b9c8e2785e1d15935568310f6d
Cavan Natal <82979308+cavannatal@users.noreply.github.com>
2024-10-02 18:38:27 -0500

    Added Fonts

--- src/pages/HomePage/components/ImageDisplay.tsx
+++ src/pages/HomePage/components/ImageDisplay.tsx
@@ -7,8 +7,9 @@ import img5 from '../images/img5.png';
 import img6 from '../images/img6.png';
 import img7 from '../images/img7.png';
 import img8 from '../images/img8.png';
+import img9 from '../images/img9.png';
 
-const images = [img1, img2, img3, img4, img5, img6, img7, img8];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9];
 
 const ImageScrollBar: React.FC = () => {
     return (
